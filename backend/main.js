de85269656d827130d3307912ab4f45f6057bd96
vijaynimar <vijaynimar8@gmail.com>
2024-12-19 18:16:30 +0530

    update

--- backend/main.js
+++ backend/main.js
@@ -7,11 +7,18 @@ const Port=2999
 connection()
 
 app.use(cors({
-    origin: "https://fitness-buddy-app.onrender.com",
-    methods: ["GET", "POST", "PUT", "DELETE"],  
-    // allowedHeaders: ["Content-Type", "Authorization"],
-    credentials: true 
+    origin: ["https://fitness-buddy-app.onrender.com", "http://localhost:5173"], 
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+    credentials: true
 }));
+
+
+
+// const cors = require('cors'); app.use(cors({ origin: [ "http://localhost:5173", // Allow localhost during development "https://fitness-buddy-app.onrender.com" // Allow production domain ], methods: ["GET", "POST", "PUT", "DELETE"], allowedHeaders: ["Content-Type", "Authorization"], credentials: true, // Allows cookies or headers to be sent along with requests }));
+
+
+
 
 
 
