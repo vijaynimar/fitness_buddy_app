5a206e8fccacc738942b8c38230b7237ccb5e859
vijaynimar <vijaynimar8@gmail.com>
2024-12-20 19:48:29 +0530

    delete-workout-on-every-monday

--- backend/main.js
+++ backend/main.js
@@ -1,6 +1,8 @@
 import express from "express"
 import connection from "./db.js"
 import { userRouter } from "./routers/router.js"
+import { workout } from "./models/workoutTracking.js"
+import cron from "node-cron"
 import cors from "cors"
 const app=express()
 const Port=2999
@@ -12,7 +14,13 @@ app.use(cors({
     allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true
 }));
-
+cron.schedule("1 0 * * 1",async()=>{
+    try{
+        await workout.deleteMany({})
+    }catch(err){
+        console.log("error in node:cron");
+    }
+})
 
 
 // const cors = require('cors'); app.use(cors({ origin: [ "http://localhost:5173", // Allow localhost during development "https://fitness-buddy-app.onrender.com" // Allow production domain ], methods: ["GET", "POST", "PUT", "DELETE"], allowedHeaders: ["Content-Type", "Authorization"], credentials: true, // Allows cookies or headers to be sent along with requests }));
